a9166c6d4a367e065e9112ebed1a8d86d8f06b87
leela khadka <khadkaleela3@gmail.com>
2022-10-14 10:30:58 -0700

    commit tree e69d83c589b78b519a44e6132eef71d79fb44d0e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+   validates :title, presence: true
+   #validates :length: { minimum: 250 }
+  # validates :summary, length: { maximun: 250 }
+  
+  
+  
+  # ost content is at least 250 characters long
+#6. Post summary is a maximum of 250 characters
+#7. Post category is either `Fiction` or `Non-Fiction
 end
